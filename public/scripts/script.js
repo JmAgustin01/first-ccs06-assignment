9009f3473a9340ccc7db1ad368aa2736a135dd8f
josh manalang <manalang.josh@auf.edu.ph>
2025-01-29 18:05:19 +0800

    first commit

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,31 +1,34 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+  console.log("ready!");
 
-    $('.generate-dog').click(() => {
-        $.ajax({
-          url: '/fetch_dog', // URL of the Laravel endpoint
+  $('.generate-dog').click(() => {
+      const selectedBreed = $('#for-breads').val(); // Get the selected breed
+
+      $.ajax({
+          url: '/fetch_dog', // Laravel endpoint
           type: 'GET',
-          success: function(response) {
-            if (response.image_url) {
-                // Clear previous images
-                $('#image-container').html('');
+          data: { breed: selectedBreed }, // Send the selected breed as a query parameter
+          success: function (response) {
+              if (response.image_url) {
+                  // Clear previous images
+                  $('#image-container').html('');
 
-                // Create a new img element with the fetched URL
-                const imgElement = $('<img>').attr('src', response.image_url).attr('alt', 'Random Dog');
+                  // Create a new img element with the fetched URL
+                  const imgElement = $('<img>')
+                      .attr('src', response.image_url)
+                      .attr('alt', 'Random Dog');
 
-                // Append the img element to the container
-                $('#image-container').append(imgElement);
-            } else {
-                console.error('Failed to fetch image:', response.error);
-            }
+                  // Append the img element to the container
+                  $('#image-container').append(imgElement);
+              } else {
+                  console.error('Failed to fetch image:', response.error);
+              }
 
-            console.log(response);
-
+              console.log(response);
           },
-          error: function(xhr) {
-            console.error('Error:', xhr.responseText);
+          error: function (xhr) {
+              console.error('Error:', xhr.responseText);
           }
-        });
       });
-
+  });
 });
